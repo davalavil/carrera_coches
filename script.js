7b89163c1e8e64dd0684ae978108b4f95775086b
davalavil <47474842+davalavil@users.noreply.github.com>
2025-04-10 07:24:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const TURN_SPEED = 0.055;
 
 // --- Dimensiones PISTA FIGURA 8 ---
 const TRACK_OUTER_W = 100;
-const TRACK_OUTER_H = 100;
+const TRACK_OUTER_H = 130;
 const TRACK_THICKNESS = 18;
 const HOLE_H = TRACK_OUTER_H - (2 * TRACK_THICKNESS);
 const HOLE_W = (TRACK_OUTER_W - (3 * TRACK_THICKNESS)) / 2;
